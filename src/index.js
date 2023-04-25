623d40032b9057ba7a6d8eca855aa6d022843165
Ivan Malenko <ivan.malenko18@gmail.com>
2023-04-26 01:01:44 +0300

    finished with local

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from "./redux/store";
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+
       <BrowserRouter basename="/goTest">
         <App />
     </BrowserRouter>
-  </PersistGate>
+
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,18 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 import { usersReducer } from './slice';
 
 
 
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
-
-const authPersistConfig = {
-  key: 'users',
-  storage,
-  whitelist: ["isFollowed"],
-};
-
 
 export const rootReducer = {
-        users: persistReducer(authPersistConfig, usersReducer), 
+        users: usersReducer, 
 };
 
 
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware,
-devTools: process.env.NODE_ENV === 'development',
 });
 
-export const persistor = persistStore(store);
